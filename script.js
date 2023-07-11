83552a8459324f30e5b52bdf5b54ffe629bba4a7
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-07-11 20:53:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+var a = prompt("Enter a string");
+    let result = [];
+    for(let i=0;i<a.length;i++){
+      let count=0;
+      for(let j=0;j<a.length;j++){
+        if(a.charAt(i) === a.charAt(j)){
+          count++;
+        }
+      }
+      if(count === 1){
+        result.push(a.charAt(i));
+      }
+    }
+    alert(result[0]);
